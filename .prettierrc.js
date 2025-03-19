08e5be38e278b2e1a6986172f5637d9ae5905596
TSenter <tyler.senter.427@gmail.com>
2025-03-19 16:32:19 -0400

    feat: Setup template imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,15 @@
 'use strict';
 
 module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
+  singleQuote: true,
   overrides: [
     {
-      files: '*.{js,ts}',
+      parser: 'ember-template-tag',
+      files: '*.{js,ts,gjs,gts}',
       options: {
         singleQuote: true,
+        templateSingleQuote: false,
       },
     },
   ],
