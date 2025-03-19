f34c6c501dd93d8b9329c135e0afb8f05e7620d7
TSenter <tyler.senter.427@gmail.com>
2025-03-19 17:00:21 -0400

    feat: Add Sidebar sample

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,7 +3,10 @@ import '@glint/environment-ember-template-imports';
 
 import PageTitleRegistry from 'ember-page-title/template-registry';
 
+import Sample from './components/sample.gts';
+
 declare module '@glint/environment-ember-loose/registry' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export default interface Registry extends PageTitleRegistry {}
+  export default interface Registry extends PageTitleRegistry {
+    Sample: typeof Sample;
+  }
 }
