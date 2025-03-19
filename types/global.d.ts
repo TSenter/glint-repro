91836b16571ed4c23bb398afdf1c15424169b7a6
TSenter <tyler.senter.427@gmail.com>
2025-03-19 16:50:51 -0400

    fix: Resolve ESLint issues

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,5 +4,6 @@ import '@glint/environment-ember-template-imports';
 import PageTitleRegistry from 'ember-page-title/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export default interface Registry extends PageTitleRegistry {}
 }
