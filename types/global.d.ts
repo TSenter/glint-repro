cfe8c967636f302f1403ad29262ce2843d50c3c4
TSenter <tyler.senter.427@gmail.com>
2025-03-19 16:36:42 -0400

    feat: Setup Glint for template imports

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,8 @@
 import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
+
+import PageTitleRegistry from 'ember-page-title/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends PageTitleRegistry {}
+}
